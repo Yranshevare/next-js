e807e75875cd394303e0b5177f79738d11deac8c
yadnesh ranshevare <yranshevare2005@gmail.com>
2024-12-22 19:59:20 +0530

    add comment

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDb() {
     try {
-        const connectionInstance = mongoose.connect(process.env.MONGODB_URL!);
+        const connectionInstance = mongoose.connect(process.env.MONGODB_URL!);      //! sure that process.env.MONGODB_URL will not be null or undefined at runtime 
         console.log("mongodb connected successfully")
     } catch (error) {
         console.log("error while connecting to database",error)

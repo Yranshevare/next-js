71f070fdc88fa7b3b82daf9c877310758f338f78
yadnesh ranshevare <yranshevare2005@gmail.com>
2024-12-23 15:51:01 +0530

    minor changes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import axios from 'axios'
-import { tree } from 'next/dist/build/templates/app-page'
 
 export default function page() {
   const router = useRouter()

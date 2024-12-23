2737b58aacff49f23eb8b74fcb8d122a2b701630
yadnesh ranshevare <yranshevare2005@gmail.com>
2024-12-23 13:26:32 +0530

    done with login sign up frontend component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
 
 export default function page() {
+  const [user,setUser] = useState({
+    email:"",
+    password:"",
+  })
+
+  const onLogin = async() => {
+
+  }
+
+
   return (
-    <div>
-      please log in
+    <div className='flex flex-col item-center justify-center min-h-screen py-2 px-16'>
+      <h1>Login</h1>
+      <br />
+
+      <label htmlFor="email">email</label>
+      <input 
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        type="text" 
+        id='email' 
+        value={user.email} 
+        onChange={(e)=>setUser({...user,email:e.target.value})} 
+        placeholder='email' 
+      />
+
+      <label htmlFor="password">password</label>
+      <input 
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        type="password" 
+        id='password' 
+        value={user.password} 
+        onChange={(e)=>setUser({...user,password:e.target.value})} 
+        placeholder='password' 
+      />
+
+      <button 
+        onClick={onLogin}
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+      >login</button>
+
+      <Link href="/signup">visit signup</Link>
     </div>
   )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
 
 export default function page() {
+  const [user,setUser] = useState({
+    email:"",
+    password:"",
+    username:""
+  })
+
+  const onSignup = async() => {
+
+  }
+
+
   return (
-    <div>
-      sign up
+    <div className='flex flex-col item-center justify-center min-h-screen py-2 px-16'>
+      <h1>signup</h1>
+      <br />
+      
+      <label htmlFor="username">username</label>
+      <input 
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        type="text" 
+        id='username' 
+        value={user.username} 
+        onChange={(e)=>setUser({...user,username:e.target.value})} 
+        placeholder='username' 
+      />
+
+      <label htmlFor="email">email</label>
+      <input 
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        type="text" 
+        id='email' 
+        value={user.email} 
+        onChange={(e)=>setUser({...user,email:e.target.value})} 
+        placeholder='email' 
+      />
+
+      <label htmlFor="password">password</label>
+      <input 
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        type="password" 
+        id='password' 
+        value={user.password} 
+        onChange={(e)=>setUser({...user,password:e.target.value})} 
+        placeholder='password' 
+      />
+
+      <button 
+        onClick={onSignup}
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+      >signup</button>
+
+      <Link href="/login">visit login</Link>
     </div>
   )
 }
